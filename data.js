110d29e573b769963d5710aa4547695d4adaaae0
recepkeskin10 <keskinrecep26@gmail.com>
2025-05-07 15:49:47 +0300

    Marzipano dosyaları eklendi

---
+++ data.js
@@ -0,0 +1,101 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_fly_20250507_124248_239_1746611201161_pano",
+      "name": "dji_fly_20250507_124248_239_1746611201161_pano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.46010071334879576,
+        "pitch": 0.04204971170682015,
+        "fov": 1.222445190334001
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07384385605298505,
+          "pitch": 0.09910692412672084,
+          "rotation": 0,
+          "target": "1-dji_fly_20250507_124546_240_1746611188191_pano"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07331404762274119,
+          "pitch": -0.034782842401615,
+          "title": "Salon 1",
+          "text": "Toplantı Salonu"
+        }
+      ]
+    },
+    {
+      "id": "1-dji_fly_20250507_124546_240_1746611188191_pano",
+      "name": "dji_fly_20250507_124546_240_1746611188191_pano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.40860781162018256,
+          "pitch": 0.333776629052549,
+          "rotation": 0,
+          "target": "0-dji_fly_20250507_124248_239_1746611201161_pano"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.40936296529044824,
+          "pitch": 0.20251559720376378,
+          "title": "Salon 2",
+          "text": "Giriş Hol"
+        }
+      ]
+    }
+  ],
+  "name": "ArsaraziOfis",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
